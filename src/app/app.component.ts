135f0be1166e05387088401c78f9d74d26397aed
Maxime <javeuch@gmail.com>
2021-04-21 06:08:36 +0200

    comportement infini avant interface 'OnDestroy' avec méthode 'unsubscribe'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AppareilService } from './services/appareil.services';
+/*  l'observable s'importe comme ceci depuis RxJS6 */
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit {
+  /* Attributs */
+  secondes: number;
 
-   }
+  ngOnInit() {
+    /* Déclaration d'un compteur qui envoie un nombre à interval de 1000 ms */
+    const counter = interval(1000);
+
+    /* On récupère les valeurs de l'observable en souscrivant */
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      /* ici, subscribe() ne les déclenche pas -------------*/
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+      /*----------------------------------------------------*/
+    );
+  }
 }
